3f61934e81e768bd132be3b289a7c89477d0a0d4
Arun <Arun@192.168.1.36>
2018-04-21 11:20:28 +0530

    checkin test 1

--- Check_Core_Java/src/checking_concepts/Hello_World.java
+++ Check_Core_Java/src/checking_concepts/Hello_World.java
@@ -42,6 +42,7 @@ public class Hello_World {
 			Transport.send(message);
 
 			System.out.println("Done");
+		//	System.out.println("checking test");
 
 		} catch (MessagingException e) {
 			throw new RuntimeException(e);
