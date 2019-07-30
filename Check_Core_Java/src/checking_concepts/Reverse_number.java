a31e6c06c831b5b0426b7af0f76b354a98b45403
Arun <Arun@192.168.4.106>
2019-07-30 13:36:01 +0530

    commit them

--- Check_Core_Java/src/checking_concepts/Reverse_number.java
+++ Check_Core_Java/src/checking_concepts/Reverse_number.java
@@ -5,8 +5,8 @@ public class Reverse_number {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int num=1990;
-		int rev=0;
+		int num=1234;
+	//	int rev=0;
 			while (num>0) {
 			int rem=num%10;
 			System.out.print(rem);
